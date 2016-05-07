7ee4678cafae55bdd7d3a64c1b2b26acf5551a7d
balzss <sarosbazsi@gmail.com>
2016-05-07 11:50:32 +0200

    $home

--- push_dots.sh
+++ push_dots.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+home=$HOME
 cp ~/.vimrc ~/dotfiles/.vimrc
 cp ~/.zshrc ~/dotfiles/.zshrc
 cp ~/.Xresources ~/dotfiles/.Xresources
 cp ~/.tmux.conf ~/dotfiles/.tmux.conf
 git add --all
-git commit -m '$HOME'
+git commit -m '$home'
 git push
