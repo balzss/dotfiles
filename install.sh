7614c41efcdab5eec03f56809da69c3f92f1f5a4
balazs.saros <balazs.saros@gmail.com>
2016-08-14 22:21:22 +0200

    new font in xres, added copy feature to tmux

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ln -s ~/dotfiles/.vimrc .vimrc
-ln -s ~/dotfiles/.zshrc .zshrc
-ln -s ~/dotfiles/.Xresources .Xresources
-ln -s ~/dotfiles/.tmux.conf .tmux.conf
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.Xresources ~/.Xresources
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
