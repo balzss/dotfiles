9a266d99023dd21196181b87a78abd44becfb43f
balazs.saros <balazs.saros@gmail.com>
2016-09-21 16:37:19 +0200

    removed deprecated README

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.Xresources ~/.Xresources
-ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.yaourtrc ~/.yaourtrc
-ln -s ~/dotfiles/prompt_balzss_setup ~/.zprezto/modules/prompt/prompt_balzss_setup
-ln -s ~/dotfiles/.zpreztorc ~/.zpreztorc
