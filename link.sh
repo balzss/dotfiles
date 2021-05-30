a33d752440255e44e6b5f19ba1bae98feb0fdadb
balazs.saros <balazs.saros@gmail.com>
2021-05-30 23:50:28 +0200

    add lazygit config

--- link.sh
+++ link.sh
@@ -5,3 +5,10 @@ ln -isv $HOME/dotfiles/zsh/.zshenv $HOME/.zshenv
 ln -isv $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -isv $HOME/dotfiles/gitconfig/.gitconfig $HOME/.gitconfig
 ln -isv $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
+
+case "$(uname)" in
+  Linux) LAZYGIT_TARGET_DIR="$HOME/.config/lazygit/config.yml";;
+  Darwin) LAZYGIT_TARGET_DIR="$HOME/Library/Application Support/lazygit/config.yml";;
+  *) ;;
+esac
+ln -isv $HOME/dotfiles/lazygit/config.yml $LAZYGIT_TARGET_DIR
